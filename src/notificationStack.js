04a6fcd9930f806b407469a2fd1d7d4f497e7095
Patrick Burtchaell <patrick@pburtchaell.com>
2016-03-04 08:29:52 -0600

    Add getStyle function NotificationStack
    This function resolves the issue causing custom styles to be ignored
    when using the NotificationStack.

--- src/notificationStack.js
+++ src/notificationStack.js
@@ -7,33 +7,54 @@ import StackedNotification from './stackedNotification';
  * pure function here. It just needs to return the stacked array
  * of notification components.
  */
-const NotificationStack = props => (
-  <div className="notification-list">
-    {props.notifications.map((notification, index) => {
-      const dismissAfter = notification.dismissAfter || props.dismissAfter || 2000;
-      const lastNotificationDismissAfter = 300;
-      const isLast = index === 0 && props.notifications.length === 1;
-      return (
-        <StackedNotification
-          {...notification}
-          key={notification.key}
-          isLast={isLast}
-          action={notification.action || props.action}
-          dismissAfter={isLast ? dismissAfter : dismissAfter + (index * 1000)}
-          onClick={() => props.onDismiss(notification)}
-          onDismiss={() => setTimeout(() => {
-            setTimeout(props.onDismiss.bind(this, notification), lastNotificationDismissAfter);
-          }, dismissAfter)}
-          style={notification.style !== null ? notification.style : {
-            bar: {
-              bottom: `${2 + index * 4}rem`,
-            }
-          }}
-        />
-      );
-    })}
-  </div>
-);
+const NotificationStack = props => {
+  let getStyle = function (notification, index) {
+    const value = `${2 + index * 4}rem`;
+
+    if (!notification.style) {
+      return {
+        bar: {
+          bottom: value
+        }
+      };
+    } else {
+      return {
+        bar: {
+          ...notification.style.bar,
+          bottom: value
+        },
+        action: {
+          ...notification.style.action
+        }
+      };
+    }
+  }
+
+  return (
+    <div className="notification-list">
+      {props.notifications.map((notification, index) => {
+        const dismissAfter = notification.dismissAfter || props.dismissAfter || 2000;
+        const lastNotificationDismissAfter = 300;
+        const isLast = index === 0 && props.notifications.length === 1;
+
+        return (
+          <StackedNotification
+            {...notification}
+            key={notification.key}
+            isLast={isLast}
+            action={notification.action || props.action}
+            dismissAfter={isLast ? dismissAfter : dismissAfter + (index * 1000)}
+            onClick={() => props.onDismiss(notification)}
+            onDismiss={() => setTimeout(() => {
+              setTimeout(props.onDismiss.bind(this, notification), lastNotificationDismissAfter);
+            }, dismissAfter)}
+            styles={getStyle(notification, index)}
+          />
+        );
+      })}
+    </div>
+  )
+};
 
 NotificationStack.propTypes = {
   notifications: PropTypes.array.isRequired,
