71d593994a53562557c9b8cb7db235845c33df91
Patrick Burtchaell <patrick@pburtchaell.com>
2016-03-16 19:24:11 -0500

    Delete unused imports

--- src/notification.js
+++ src/notification.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import defaultPropTypes from './defaultPropTypes';
 
 class Notification extends Component {
