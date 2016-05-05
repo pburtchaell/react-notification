e207c36f54b19e3b21d9cf42dea699c8f9a41f7c
Lionel T <elrumordelaluz@hotmail.com>
2016-05-06 01:10:07 +0200

    Update and add Tests
    - Use mount() and done() for async events
    - Add tests to ensure callback function is fired after and not before the
      timer and transition + accumulated time (for Stacked notifications)

--- test/notificationStack.js
+++ test/notificationStack.js
@@ -2,19 +2,57 @@ import { Notification, NotificationStack } from '../src/index';
 import mockNotification from './mockNotification';
 
 describe('<NotificationStack />', () => {
-  it('onDismiss fires once', () => {
+  let notifications;
+
+  beforeEach(() => {
+    notifications = [
+      mockNotification,
+      Object.assign({}, mockNotification, { key: 2222 })
+    ];
+  });
+
+  it('onDismiss fires after `dismissAfter` value + transition time', done => {
     const handleDismiss = spy();
 
-    const wrapper = shallow(
+    const wrapper = mount(
       <NotificationStack
-        notifications={[mockNotification]}
+        notifications={notifications}
         onDismiss={handleDismiss}
       />
     );
-
+    
+    wrapper.update();
     setTimeout(() => {
-      expect(handleDismiss.calledOnce).to.equal(true);
-    }, mockNotification.dismissAfter);
+      try {
+        expect(handleDismiss.calledOnce).to.equal(true);
+        done();
+      } catch (e) {
+        done(e);
+      }
+      // Add time due to each StackedNotification transition time ( > 300 )
+    }, mockNotification.dismissAfter + 340);
+  });
+
+  it('onDismiss fires on each Notification in the stack', done => {
+    const handleDismiss = spy();
+
+    const wrapper = mount(
+      <NotificationStack
+        notifications={notifications}
+        onDismiss={handleDismiss}
+      />
+    );
+    
+    wrapper.update();
+    setTimeout(() => {
+      try {
+        expect(handleDismiss.callCount).to.equal(notifications.length);
+        done();
+      } catch (e) {
+        done(e);
+      }
+      // Add transition time + 1000ms per each Notification 
+    }, mockNotification.dismissAfter + 1340);
   });
 
   it('onDismiss does not fire until `dismissAfter` value times out', () => {
--- test/setup.js
+++ test/setup.js
@@ -9,5 +9,22 @@ global.React = require('react');
 global.expect = require('chai').expect;
 global.spy = require('sinon').spy;
 global.shallow = require('enzyme').shallow;
+global.mount = require('enzyme').mount;
+global.jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
 
 chai.use(chaiEnzyme());
