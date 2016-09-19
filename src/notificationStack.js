e544b2ee0a419b0a712b0968b8b0f39576bc9f94
Joey Figaro <joey@joeyfigaro.com>
2016-09-19 12:11:13 -0400

    fixed lint issues in notificationStack

--- src/notificationStack.js
+++ src/notificationStack.js
@@ -3,11 +3,11 @@ import React, { PropTypes } from 'react';
 import StackedNotification from './stackedNotification';
 
 function defaultStyleFactory(index, style) {
-  return Object.assign(
-    {},
-    style,
-    { bottom: `${2 + index * 4}rem` }
-  );
+    return Object.assign(
+        {},
+        style,
+        { bottom: `${2 + index * 4}rem` }
+    );
 }
 
 /**
@@ -18,41 +18,38 @@ function defaultStyleFactory(index, style) {
 const NotificationStack = props => (
     <div className="notification-list">
       {props.notifications.map((notification, index) => {
-        const dismissAfter = notification.dismissAfter || props.dismissAfter;
-        const isLast = index === 0 && props.notifications.length === 1;
-        const barStyle = props.barStyleFactory(index, notification.barStyle);
-        const activeBarStyle = props.activeBarStyleFactory(
-          index,
-          notification.activeBarStyle
-        );
+          const dismissAfter = notification.dismissAfter || props.dismissAfter;
+          const isLast = index === 0 && props.notifications.length === 1;
+          const barStyle = props.barStyleFactory(index, notification.barStyle);
+          const activeBarStyle = props.activeBarStyleFactory(index, notification.activeBarStyle);
 
-        return (
-          <StackedNotification
-            {...notification}
-            key={notification.key}
-            isLast={isLast}
-            action={notification.action || props.action}
-            dismissAfter={isLast ? dismissAfter : dismissAfter + (index * 1000)}
-            onDismiss={props.onDismiss.bind(this, notification)}
-            activeBarStyle={activeBarStyle}
-            barStyle={barStyle}
-          />
+          return (
+            <StackedNotification
+                {...notification}
+                key={notification.key}
+                isLast={isLast}
+                action={notification.action || props.action}
+                dismissAfter={isLast ? dismissAfter : dismissAfter + (index * 1000)}
+                onDismiss={props.onDismiss.bind(this, notification)}
+                activeBarStyle={activeBarStyle}
+                barStyle={barStyle}
+            />
         );
       })}
     </div>
 );
 
 NotificationStack.propTypes = {
-  activeBarStyleFactory: PropTypes.func,
-  barStyleFactory: PropTypes.func,
-  notifications: PropTypes.array.isRequired,
-  onDismiss: PropTypes.func.isRequired,
+    activeBarStyleFactory: PropTypes.func,
+    barStyleFactory: PropTypes.func,
+    notifications: PropTypes.array.isRequired,
+    onDismiss: PropTypes.func.isRequired,
 };
 
 NotificationStack.defaultProps = {
-  dismissAfter: 1000,
-  activeBarStyleFactory: defaultStyleFactory,
-  barStyleFactory: defaultStyleFactory,
+    dismissAfter: 1000,
+    activeBarStyleFactory: defaultStyleFactory,
+    barStyleFactory: defaultStyleFactory,
 };
 
 export default NotificationStack;
