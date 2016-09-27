cf8c784a63b1e92790b14244c8cc6ddb27e67ead
Joey Figaro <joey@joeyfigaro.com>
2016-09-27 09:33:04 -0400

    fixed jsx indenting, eslint rule, and src/index exports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export * from './notification';
-export * from './notificationStack';
+export { default as Notification } from './notification';
+export { default as NotificationStack } from './notificationStack';
