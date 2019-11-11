bce321ece68c664195cc421e5d01ff2c4055a3c1
Patrick Burtchaell <patrick@pburtchaell.com>
2019-11-11 09:39:11 -0600

    Rename example

--- examples/multiple-notifications/index.js
+++ examples/multiple-notifications/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { render } from 'react-dom'
-import { NotificationStack } from '../../src/index.js'
+import { NotificationStack } from '../../src/index.jsx'
 
 function Example() {
   return null
